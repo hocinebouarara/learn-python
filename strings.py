3f6d2087358aa3b58ecba259892cf81c158cd589
hocinebouarara <hocine@univ-tiaret.dz>
2021-12-04 09:00:15 +0100

    string concatination

--- strings.py
+++ strings.py
@@ -5,33 +5,34 @@ it is best for learn web programing
 .......
 """
 
-print(a) # >>> i use W3schools.com for learning 
-         # it is best for learn web programing
-         #.......
+print(a)  # >>> i use W3schools.com for learning
+# it is best for learn web programing
+# .......
 
 a = "Hello World!"
-print(a[0])    # >>> H 
+print(a[0])    # >>> H
 
 for x in "banana":
-    print(x) # >>> b
-             # a
-             # n
-             # a
-             # n
-             # a
+    print(x)  # >>> b
+    # a
+    # n
+    # a
+    # n
+    # a
 a = "hello world!"
-print(len(a))  # >>> 12 
+print(len(a))  # >>> 12
 
 txt = "The best things in life are free"
 print("free" in txt)   # >>> True
 
 if "free" in txt:
-    print("Yes, 'free' is present") # >>> Yes, 'free' is present
+    print("Yes, 'free' is present")  # >>> Yes, 'free' is present
 
 print("expensive" not in txt)   # >>> True
 
 if "expensive" not in txt:
-    print("No, 'expensive' is NOT present.") # >>> No 'exepensive' is NOT present.
+    # >>> No 'exepensive' is NOT present.
+    print("No, 'expensive' is NOT present.")
 
 # Slicing Strings
 
@@ -50,19 +51,18 @@ print(x.lower())   # >>> hocine bouarara
 x = "Hocine   Bouarara  "
 print(x.strip())   # >>> hocine bouarara
 
-print(x.replace('a','A')) # >>> hocine bouArArA
+print(x.replace('a', 'A'))  # >>> hocine bouArArA
 x = "Hocine, Bouarara"
 
 print(x.split(","))  # >>> ['Hocine','Bouarara']
 
 
+#  String Concatination
 
+firstname = "hocine"
+lastname = "bouarara"
+username = firstname + lastname
+print("The user name is : ", username)   # The user name is : hocinebouarara
 
-  
-
-
-
-
-
-
-
+name=firstname+" "+lastname
+print("The name is : ",name)     # The name is : hocine bouarara
