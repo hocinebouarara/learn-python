e104a6d42a5dbbd698c515d5cca3f5c39dd95a45
hocinebouarara <hocine@univ-tiaret.dz>
2021-12-04 08:30:21 +0100

    Strings

---
+++ strings.py
@@ -0,0 +1,36 @@
+print("Hello world!")  # >>> Hello world!
+
+a = """ i use W3schols for learning 
+it is best for learn web programing
+.......
+"""
+
+print(a) # >>> i use W3schools.com for learning 
+         # it is best for learn web programing
+         #.......
+
+a = "Hello World!"
+print(a[0])    # >>> H 
+
+for x in "banana":
+    print(x) # >>> b
+             # a
+             # n
+             # a
+             # n
+             # a
+a = "hello world!"
+print(len(a))  # >>> 12 
+
+txt = "The best things in life are free"
+print("free" in txt)   # >>> True
+
+if "free" in txt:
+    print("Yes, 'free' is present") # >>> Yes, 'free' is present
+
+print("expensive" not in txt)   # >>> True
+
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.") # >>> No 'exepensive' is NOT present.
+
+
