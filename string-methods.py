346d3e632cc9f098c4a559f36c9c7fba0523fe4e
hocinebouarara <hocine@univ-tiaret.dz>
2021-12-04 11:04:13 +0100

    casefold and center string method and count of word or char in string

--- string-methods.py
+++ string-methods.py
@@ -2,3 +2,13 @@ name = 'hocine'
 # converts the first character to upper case
 output = name.capitalize()
 print(output)  # >>> Hocine
+
+name = "Hocine BOUARARA"
+# Converts string int lower case
+output= name.casefold()
+print(output)  # >>> hocine bouarara
+
+# Returns a centered string but we give it nbr spaces
+print(name.center(30))
+
+print(output.count('a'))
