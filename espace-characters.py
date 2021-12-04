bf70ceb422808bc27ab1e2a88c470d1e1af2f38e
hocinebouarara <hocine@univ-tiaret.dz>
2021-12-04 10:41:22 +0100

    espace characters

---
+++ espace-characters.py
@@ -0,0 +1,30 @@
+txt = "My name is hocine i am from \"Algeria\""
+print(txt)  # >>> My name is hocine i am from "Algeria"
+
+txt = 'my name is \'hocine\' i am 26'
+print(txt)  # >>> my name is \'hocine\' i am 26
+
+txt = "We study with W3schools.com,\nwe learn web develop. "
+print(txt)  # >>> We study with W3schools.com,
+# We learn web develop.
+
+txt = "hocien\rbouarara"
+print(txt)  # >>> bouarara
+
+txt = "Hello\tTab!"
+print(txt) # >>> Hello  Tab! 
+
+txt = "hocine \b bouarara"
+print(txt) # >>> hocinebouarara
+
+txt = "hocine \f bouarara"
+print(txt) # >>> hocine
+           #      bouarara
+
+txt = "\110\145\154\154\157"
+print(txt) # >>> Hello 
+
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) # >>> Hello 
+
+
